2728199dc9d83ffcac21a0ed3a175203aecd1295
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-06 12:50:46 -0500

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -1,9 +1,25 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
+import pydeck as pdk
 
-DATA_URL = ("https://github.com/mpellet20/Streamlit/blob/main/fonds_fmoq.csv")
+# SETTING PAGE CONFIG TO WIDE MODE
+st.beta_set_page_config(layout="wide")
 
-data = pd.read_csv(DATA_URL)
+# LOADING DATA
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
 
 data = load_data(100000)
-data
+

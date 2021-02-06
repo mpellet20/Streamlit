3b8a809c87655239fd186be74ab26c410db52878
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-06 12:48:01 -0500

    Create Uber_pickup_demo.py

---
+++ Uber_pickup_demo.py
@@ -0,0 +1,26 @@
+"""An example of showing geographic data."""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
+
+# SETTING PAGE CONFIG TO WIDE MODE
+st.beta_set_page_config(layout="wide")
+
+# LOADING DATA
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+data = load_data(100000)

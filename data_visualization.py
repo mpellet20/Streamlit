3dbdd6e71e6ce7dcfa948763e90844951f3692b1
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-06 12:40:28 -0500

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -3,7 +3,7 @@ import numpy as np
 
 DATA_URL = ("https://github.com/mpellet20/Streamlit/blob/main/fonds_fmoq.csv")
 
-data = pd.read_csv(DATA_URL, nrows=nrows)
+data = pd.read_csv(DATA_URL)
 
 data = load_data(100000)
 data

c2e0e8efabc643ca8b3258fe5d4e781fc716cdc5
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-09 19:44:25 -0500

    Create wtg01.py

---
+++ wtg01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 16:55:08 2021
+
+@author: mpell
+"""
+
+
+import pandas as pd
+
+wind_data = pd.read_csv('wtg01.csv', names=['Date','Time','AC_POWER','WIND_SPEED'])
+
+#print(wind_data)
+
+print(wind_data.groupby('AC_POWER').WIND_SPEED.sum())
+
+print(wind_data.groupby('Date').WIND_SPEED.sum())
+
+AC_POWER = wind_data.groupby('AC_POWER').WIND_SPEED.sum()

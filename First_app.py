adb75a4749ca8a78c36f1c859ec8303a8d94030a
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-13 15:17:07 -0500

    Update First_app.py

--- First_app.py
+++ First_app.py
@@ -21,7 +21,6 @@ if st.checkbox('Show Raw Data'):
     #wind_speed = st.slider('WIND_SPEED', 0, 23, 1)
     #wind_date = st.slider('Date', '01/01/21', '23/01/2021')
     d = st.date_input("Please, enter a date")
-    st.write('Your birthday is:', d)
    
     wind_data['Date'] = d
     

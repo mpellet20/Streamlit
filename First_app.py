1986b0e9c51049744ecc881277a5f6ff5e3c54f2
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-13 09:36:01 -0500

    Update First_app.py

--- First_app.py
+++ First_app.py
@@ -6,13 +6,13 @@ import pandas as pd
 wind_data = pd.read_csv('WTG01.csv')
 #, names=['Date', 'Time', 'AC_POWER', 'WIND_SPEED'])
 
-<div>
+
 st.title('Data on Wind Power and Wind speed')
 
 st.write("Here's the table related to the wind power:")
 
 st.write(wind_data)
-</div>
+
 st.write('Here is the table of the Wind Speed related to the Wind Power')
 
 st.write(wind_data.groupby('AC_POWER').WIND_SPEED.sum())
@@ -24,8 +24,8 @@ st.write(wind_data.groupby('Date').WIND_SPEED.sum())
 wind_data = pd.read_csv('WTG01.csv')
 
 chart_data = wind_data.groupby('WIND_SPEED').AC_POWER.sum()
-<div>
+
 st.title('Here is the graphic of the AC_POWER related to the wind speed')
 
 st.line_chart(chart_data)
-</div>
+

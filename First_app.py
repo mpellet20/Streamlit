a0a808dcfb80b6da8550a5505d61c9a67af809ea
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-13 14:59:53 -0500

    Update First_app.py

--- First_app.py
+++ First_app.py
@@ -19,7 +19,7 @@ st.line_chart(chart_data)
 if st.checkbox('Show Raw Data'):
     wind_data = load_data(10000)
     #wind_speed = st.slider('WIND_SPEED', 0, 23, 1)
-    wind_date = st.slider('Date', '01/01/21', '23/01/2021',1)
+    wind_date = st.slider('Date', '01/01/21', '23/01/2021')
     wind_data['Date'] = wind_date
     
     st.title('Data on Wind Power and Wind speed')

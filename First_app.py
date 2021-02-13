ae96a1de09a766fa39d7442232460681ae5ea247
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-13 10:21:47 -0500

    Update First_app.py

--- First_app.py
+++ First_app.py
@@ -6,8 +6,10 @@ import pandas as pd
 #@st.cache(persist=TRUE)
 wind_data = pd.read_csv('WTG01.csv')
 
-wind_speed_sd = st.slider('WIND_SPEED', 0, 23, 10)
-wind_data = wind_data.WIND_SPEED == wind_speed_sd
+#wind_speed_sd = st.slider('WIND_SPEED', 0, 23, 10)
+wind_speed = 6
+wind_data = wind_data[wind_data]]
+'data', wind_data
 
 
 #wind_data = wind_data.wind_speed == wind_speed]

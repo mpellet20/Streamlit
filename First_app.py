f1263868b1894d774dd4ce599bd46f4253098886
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-14 09:05:01 -0500

    Update First_app.py

--- First_app.py
+++ First_app.py
@@ -4,8 +4,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 10, 30)
-y = np.sin(x)
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+#x = np.linspace(0, 10, 30)
+#y = np.sin(x)
 
 plt.plot(x, y, 'o', color='black');
 

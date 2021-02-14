2e37abc5f31581a5270f17ecd95265c5fc29aedc
mpellet20 <78170255+mpellet20@users.noreply.github.com>
2021-02-14 09:35:25 -0500

    Update First_app.py

--- First_app.py
+++ First_app.py
@@ -2,17 +2,6 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
-#x = np.linspace(0, 10, 30)
-#y = np.sin(x)
-
-plt.plot(x, y, 'o', color='black');
 
 #@st.cache(persist=TRUE)
 def load_data(nrows):
